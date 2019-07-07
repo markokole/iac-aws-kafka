ad792f98a5333b667315066a5c2a475af385acc1
Marko Kljajic <marko_kole@yahoo.com>
2019-07-07 11:02:18 +0200

    added output public_dns

--- modules/instance/output.tf
+++ modules/instance/output.tf
@@ -2,9 +2,9 @@
 #  value = "${aws_instance.spark_master.*.id}"
 #}
 
-#output "public_dns" {
-#  value = "${aws_instance.spark_master.*.public_dns}"
-#}
+output "kafka_public_dns" {
+  value = "${aws_instance.kafka_cluster.*.public_dns}"
+}
 
 output "kafka_public_ip" {
   value = "${aws_instance.kafka_cluster.*.public_ip}"
